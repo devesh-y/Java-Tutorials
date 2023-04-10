59d6929db2b28bdeb79bb5c9efc9625d91f23785
Devesh Yadav <ydevesh136@gmail.com>
2023-04-10 13:13:47 +0530

    small changes

--- collections/stack in java/stacks.java
+++ collections/stack in java/stacks.java
@@ -1,6 +1,5 @@
 import java.util.Stack;
 
-
 public class stacks {
     public static void main(String[] args) {
         
--- wrapper class/wrapper_class.java
+++ wrapper class/wrapper_class.java
@@ -1,6 +1,13 @@
 
 public class wrapper_class{
     public static void main(String[] args) {
+        int i = 10;
+        Integer integer = i; // boxing happens automatically
+
+        Integer integ = new Integer(11);
+        int p = integ; // unboxing happens automatically
+        System.out.println(p);
+
         
     }
 }
