d1e9f360c2244aefdc1c8cc540f8d8782841fbfd
Devesh Yadav <ydevesh136@gmail.com>
2023-04-11 13:35:35 +0530

    change

--- collections/pair in java/pair_class.java
+++ collections/pair in java/pair_class.java
@@ -1,11 +1,11 @@
-import java.util.*;
+
 import javafx.util.*;
 public class pair_class {
     public static void main(String[] args) 
     {
 
-        Pair p1=new Pair(4, 34);
-        System.out.println(p1);
+        // Pair p1=new Pair(4, 34);
+        // System.out.println(p1);
         //dont use the above one
 
         Pair<Integer, String> p = new Pair<>(10, "Hello Geeks!");
